1fcb151b675842ceaf28bd2c3cff8eec47866c30
sylvia <sylvia_lumen@proton.me>
2023-11-09 09:17:31 +0200

    1.2

--- linkparser.py
+++ linkparser.py
@@ -1,11 +1,26 @@
 import re
-from typing import Match, List, Any
+import configparser
 
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+COURSES_ID = [["eng", "637129488148"],
+              ["math", "635843353358"],
+              ["hist", "620797854331"],
+              ["phy", "620408593297"],
+              ["ukrlit", "544956500670"]]
 
-def get_message(courses_id, creds) -> str:
+config = configparser.ConfigParser()
+config.read('/home/sylvia/.local/bin/links.ini')
+
+
+def get_message(course_id: str, creds) -> str:
+    """
+
+    :param course_id: str
+    :param creds: str
+    :return:
+    """
     try:
         """
         DONE Вынести ету консрукцию в отдельную от мейна функцию, добавив ей аргументы id: Vec<u32> и creds
@@ -19,8 +34,7 @@ def get_message(courses_id, creds) -> str:
         courses = results.get("courses", [])
 
         if courses:
-            latest_course_id = courses_id
-            announcements = service.courses().announcements().list(courseId=latest_course_id).execute()
+            announcements = service.courses().announcements().list(courseId=course_id).execute()
             if announcements.get('announcements', []):
                 latest_announcement = announcements['announcements'][0]
                 # print('Последнее сообщение в курсе: {}'.format(latest_announcement['text']))
@@ -36,9 +50,37 @@ def get_message(courses_id, creds) -> str:
     print()
 
 
-def get_link(message: str) -> str:
+def get_link(message: str, course: str) -> str:
     try:
         link = re.findall(r'https.*', message)
         return link[0]
-    except IndexError as error:
-        return "В последнем сообщении курса нет нужных ссылок"
+    except IndexError:
+        print(f"В последнем сообщении курса {course} нет нужных ссылок")
+        return " "
+
+
+def get_all_links(creds, list, id=0):
+    for i in range(len(COURSES_ID)):
+        element = []
+        link = get_link(get_message(COURSES_ID[id][1], creds), COURSES_ID[id][0])
+        element.append(COURSES_ID[id][0])
+        element.append(link)
+        if element not in list:
+            if element[0] in list:
+                list[id][1] = link
+            else:
+                list.append(element)
+        id += 1
+    print(list)
+
+
+def get_update(links, links_new, id=0):
+    if links_new != links:
+        for i in range(len(links)):
+            if links_new[id] == links[id]:
+                links[id] = links_new[id]
+                config.set("links", f"{links[id][0]}", f"{links[id][1]}")
+                with open('/home/sylvia/.local/bin/links.ini', 'w') as configfile:
+                    config.write(configfile)
+                print("save data")
+            id += 1
--- main.py
+++ main.py
@@ -4,37 +4,32 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 
-from linkparser import get_message, get_link
+from linkparser import get_message, get_link, get_all_links, get_update
 
-from flask import Flask, request
-
-app = Flask(__name__)
+import logging
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/classroom.announcements",
           "https://www.googleapis.com/auth/classroom.courses.readonly"]
 
-COURSES_ID = {"Англ": "637129488148",
-              "Математика": "635843353358",
-              "Укр мова": "620960346914",
-              "Физ-ра": "620485897294",
-              "Дискретная математика": "620844311340",
-              "История": "620797854331",
-              "Физика": "620408593297",
-              "Укр Лит": "544956500670"}
 
+links = []
+links_new = []
 
-@app.route('/webhook', methods=['POST'])
+logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO)
 def main():
     """Shows basic usage of the Classroom API.
   Prints the names of courses the user has access to.
   """
+    logging.info('Getting creds')
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+        logging.info("Creds loaded from previous session")
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -44,15 +39,18 @@ def main():
                 "credentials.json", SCOPES
             )
             creds = flow.run_local_server(port=0)
+
         # Save the credentials for the next run
         with open("token.json", "w") as token:
             token.write(creds.to_json())
+        logging.info("Creds created")
+    get_all_links(creds, links)
 
-    message = get_message(COURSES_ID["Англ"], creds)
-    print(message)
-    link = get_link(message)
-    print(link)
+    while True:
+        get_all_links(creds, links_new)
+        get_update(links, links_new)
 
 
 if __name__ == "__main__":
+    logging.info('The script is running')
     main()
